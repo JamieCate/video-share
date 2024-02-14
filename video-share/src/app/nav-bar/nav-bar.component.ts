d91e00718fa87cdf0774343abacf1f1e354b358e
unknown <hobba_25@hotmail.com>
2024-02-14 13:57:07 +0000

    add dropdown close

--- video-share/src/app/nav-bar/nav-bar.component.ts
+++ video-share/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
   showSubMenu: boolean = false;
+  constructor(private el: ElementRef) {
+  }
 
+  // Close navbar dropdown if we click anywhere
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.showSubMenu = false;
+    }
+  }
+
+  // Toggles dropdown menu
   showDropdown() {
     this.showSubMenu = !this.showSubMenu;
     console.log(this.showSubMenu);
