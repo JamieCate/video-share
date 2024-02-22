ccd47808ad90edc948297fc61096f919593ee79e
unknown <hobba_25@hotmail.com>
2024-02-22 11:43:59 +0000

    add routing for videos page

--- video-share/src/app/app.module.ts
+++ video-share/src/app/app.module.ts
@@ -6,20 +6,29 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { VideoComponentComponent } from './video-component/video-component.component';
 import { VideoService } from './services/videos.service';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
+
+const approutes: Routes = [
+  {
+    path: '',
+    component: VideoComponentComponent,
+  },
+  {
+    path: 'videos',
+    component: VideoComponentComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    VideoComponentComponent,
-  ],
+  declarations: [AppComponent, NavBarComponent, VideoComponentComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule.forRoot(approutes),
   ],
   providers: [VideoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
