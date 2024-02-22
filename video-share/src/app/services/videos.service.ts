8d18567c76e2a5065ffdf03abe2c89a9701c9b7d
unknown <hobba_25@hotmail.com>
2024-02-22 12:13:32 +0000

    clean up video page slightly

--- video-share/src/app/services/videos.service.ts
+++ video-share/src/app/services/videos.service.ts
@@ -1,66 +1,72 @@
 import { Video } from '../models/video.model';
 
 export class VideoService {
-    videos: Video[] = [
-        {
-            id: 1,
-            source:
-                'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-            title: 'test video',
-            description: 'this is the best video',
-            length: 256,
-            views: 124027,
-            favorite: true,
-        },
-        {
-            id: 2,
-            source:
-                'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-            title: 'test second',
-            description: 'this is the worst',
-            length: 326,
-            views: 123664,
-            favorite: false,
-        },
-        {
-            id: 3, // Unique ID
-            source:
-                'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-            title: 'test video',
-            description: 'this is the best video',
-            length: 256,
-            views: 124027,
-            favorite: true,
-        },
-        {
-            id: 4, // Unique ID
-            source:
-                'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-            title: 'test second',
-            description: 'this is the worst',
-            length: 326,
-            views: 123664,
-            favorite: false,
-        },
-        {
-            id: 5, // Unique ID
-            source:
-                'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-            title: 'test video',
-            description: 'this is the best video',
-            length: 256,
-            views: 124027,
-            favorite: true,
-        },
-        {
-            id: 6, // Unique ID
-            source:
-                'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-            title: 'test second',
-            description: 'this is the worst',
-            length: 326,
-            views: 123664,
-            favorite: false,
-        },
-    ];
+  videos: Video[] = [
+    {
+      id: 1,
+      source:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      title: 'Big bunny gets mad at tiny creatures in the forrest',
+      description: 'this is the best video',
+      length: 256,
+      views: 124027,
+      favorite: true,
+      uploadedBy: 'Jamie',
+    },
+    {
+      id: 2,
+      source:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      title: 'Not sure what this is but it is cool',
+      description: 'this is the worst',
+      length: 326,
+      views: 123664,
+      favorite: false,
+      uploadedBy: 'Jamie',
+    },
+    {
+      id: 3, // Unique ID
+      source:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      title: 'Big bunny gets mad at tiny creatures in the forrest',
+      description: 'this is the best video',
+      length: 256,
+      views: 124027,
+      favorite: true,
+      uploadedBy: 'Jamie',
+    },
+    {
+      id: 4, // Unique ID
+      source:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      title: 'Not sure what this is but it is cool',
+      description: 'this is the worst',
+      length: 326,
+      views: 123664,
+      favorite: false,
+      uploadedBy: 'Jamie',
+    },
+    {
+      id: 5, // Unique ID
+      source:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      title: 'Big bunny gets mad at tiny creatures in the forrest',
+      description: 'this is the best video',
+      length: 256,
+      views: 124027,
+      favorite: true,
+      uploadedBy: 'Jamie',
+    },
+    {
+      id: 6, // Unique ID
+      source:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      title: 'Not sure what this is but it is cool',
+      description: 'this is the worst',
+      length: 326,
+      views: 123664,
+      favorite: false,
+      uploadedBy: 'Jamie',
+    },
+  ];
 }
